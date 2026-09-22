e1235349774a80f40284fcba0e4ef03ef8d14cac
EndyHell <anderhellstorm@gmail.com>
2026-09-23 00:58:13 +0300

    feat: Обновление зависимости AsureLib.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,15 +11,15 @@ repositories {
     maven {
         name = "papermc"
         url = uri("https://repo.papermc.io/repository/maven-public/")
-        maven(url = "https://repo.extendedclip.com/content/repositories/placeholderapi/")
-        maven(url = "https://jitpack.io")
     }
+    maven(url = "https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:26.2+")
     compileOnly("me.clip:placeholderapi:2.11.6") // repo.extendedclip.com
-    compileOnly(files("../AsureLib/build/libs/AsureLib-1.0-SNAPSHOT.jar"))
+    compileOnly("com.github.SaintQd:AsureLib:v1.0")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 }
 
